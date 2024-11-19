7e6f837752c8b6d35f7f19687a27c2e4886d719c
tango-kshitij <guptakshitij1609@gmail.com>
2024-11-20 01:39:26 +0530

    Delete Lab-06 directory

--- Lab-06/L6Q2.py
+++
@@ -1,7 +0,0 @@
-def process_list(input_list):
-    cubed_values = list(map(lambda x: x ** 3, input_list))
-    return cubed_values
-
-my_list = [1, 2, 3, 4, 5]
-result = process_list(my_list)
-print(result)
